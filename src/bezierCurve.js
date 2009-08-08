9f0f1df8e54451160fdfc75990cb2ed5538b2d42
mollifier <mollifier@gmail.com>
2009-08-08 21:16:41 +0900

    refactoring

--- src/bezierCurve.js
+++ src/bezierCurve.js
@@ -1,18 +1,17 @@
-var BezierCurve = {};
+var BezierCurve = {
+    plusPoint : function(p1, p2) {
+        return [p1[0] + p2[0], p1[1] + p2[1]];
+    },
+    minusPoint : function(p1, p2) {
+        return [p1[0] - p2[0], p1[1] - p2[1]];
+    },
 
-BezierCurve.plusPoint = function(p1, p2) {
-    return [p1[0] + p2[0], p1[1] + p2[1]];
-};
-
-BezierCurve.minusPoint = function(p1, p2) {
-    return [p1[0] - p2[0], p1[1] - p2[1]];
-};
-
-// divide point internally to t : 1 - t
-BezierCurve.dividePoint = function(p1, p2, t) {
-    return [
-        p1[0] * t + p2[0] * (1 - t),
-        p1[1] * t + p2[1] * (1 - t)
-    ];
+    // divide point internally to t : 1 - t
+    dividePoint : function(p1, p2, t) {
+        return [
+            p1[0] * t + p2[0] * (1 - t),
+            p1[1] * t + p2[1] * (1 - t)
+        ];
+    }
 };
 
