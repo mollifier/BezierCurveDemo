6171c2c1e6415d7d10a64f360f4ad0337c4d6354
mollifier <mollifier@gmail.com>
2009-08-08 21:52:24 +0900

    Refactoring testing code

--- test/bezierCurveTest.js
+++ test/bezierCurveTest.js
@@ -11,16 +11,20 @@ testCases(test,
     },
 
     function shouldBezierCurvePlusPoint() {
-        assert.that(BezierCurve.plusPoint([1, 2], [5, 8]),
-            containsInOrder(6, 10));
+        var actual = BezierCurve.plusPoint([1, 2], [5, 8]);
+        assert.that(BezierCurve.equalPoint(actual, [6, 10]),
+            isTrue());
     },
     function shouldBezierCurveMinusPoint() {
-        assert.that(BezierCurve.minusPoint([1, 10], [5, 8]),
-            containsInOrder(-4, 2));
+        var actual = BezierCurve.minusPoint([1, 10], [5, 8]);
+        assert.that(BezierCurve.equalPoint(actual, [-4, 2]),
+            isTrue());
     },
+
     function shouldBezierCurveDividePoint() {
-        assert.that(BezierCurve.dividePoint([10, 30], [50, 90], 0.3),
-            containsInOrder(38, 72));
+        var actual = BezierCurve.dividePoint([10, 30], [50, 90], 0.3);
+        assert.that(BezierCurve.equalPoint(actual, [38, 72]),
+            isTrue());
     }
 );
 
