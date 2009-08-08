09cd9f37dae7064e1e8e803cad5efad4dd9d125c
mollifier <mollifier@gmail.com>
2009-08-08 21:18:21 +0900

    Expand tab to space

--- test/bezierCurveTest.js
+++ test/bezierCurveTest.js
@@ -1,15 +1,15 @@
 eval(loadFile("src/bezierCurve.js"));
 
 testCases(test,
-	function shouldBezierCurvePlusPoint() {
+    function shouldBezierCurvePlusPoint() {
         assert.that(BezierCurve.plusPoint([1, 2], [5, 8]),
             containsInOrder(6, 10));
-	},
-	function shouldBezierCurveMinusPoint() {
+    },
+    function shouldBezierCurveMinusPoint() {
         assert.that(BezierCurve.minusPoint([1, 10], [5, 8]),
             containsInOrder(-4, 2));
     },
-	function shouldBezierCurveDividePoint() {
+    function shouldBezierCurveDividePoint() {
         assert.that(BezierCurve.dividePoint([10, 30], [50, 90], 0.3),
             containsInOrder(38, 72));
     }
