94b6e3099965c4a20345f76d2db20d7c0f5e7df8
mollifier <mollifier@gmail.com>
2009-08-15 01:56:10 +0900

    Change to get controlPoints from input boxes.

--- src/draw.js
+++ src/draw.js
@@ -12,17 +12,32 @@ var Draw = {
         context.strokeStyle = "#0000ff";
         context.lineWidth = 3.0;
 
-        var numOfPoints = document.getElementById("numOfPoints").value;
-        numOfPoints = Draw.strToInt(numOfPoints);
-
-        var points = BezierCurve.getBezierCurvePoints(
-            numOfPoints, [10, 10], [150, 400], [350, 80]);
+        var points = Draw.getDrawPoints();
 
         context.beginPath();
         for (var i = 0; i < points.length; i++) {
             context.lineTo(points[i][0], points[i][1]);
         }
         context.stroke();
+    },
+
+    getDrawPoints: function() {
+        var ret = [];
+
+        var numOfPoints = document.getElementById("numOfPoints").value;
+        numOfPoints = Draw.strToInt(numOfPoints);
+
+        var controlPoints = [];
+
+        for (var i = 0; i <= 2; i++) {
+            var temp = document.getElementById("controlPoint" + i).value;
+            controlPoints.push(eval(temp));
+        }
+
+        ret = BezierCurve.getBezierCurvePoints(
+            numOfPoints, controlPoints[0], controlPoints[1], controlPoints[2]);
+
+        return ret;
     },
 
     strToInt: function(str) {
