dfbf9e87f1fb04b70f06b1205d7bc1b1eb7ed283
mollifier <mollifier@gmail.com>
2009-08-14 22:51:06 +0900

    Split external javascript file

---
+++ src/draw.js
@@ -0,0 +1,9 @@
+var Draw = {
+    init: function() {
+        var canvas = document.getElementById("BezierCurve");
+        canvas.innerHTML = "<b>demo js</b>";
+    }
+};
+
+window.addEventListener("load", Draw.init, false);
+
